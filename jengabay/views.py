51b68a4ca5c8e2173f5bdb463255c80313126a11
njeveh <njeveh@gmail.com>
2021-10-28 14:31:43 +0300

    added the seller api view endpoint

--- jengabay/views.py
+++ jengabay/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .serializers import *
+from rest_framework.generics import ListCreateAPIView
+from .models import *
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+class SellerListCreateView(ListCreateAPIView):
+    serializer_class = SellerSerializer
+    queryset = Seller.objects.all()
