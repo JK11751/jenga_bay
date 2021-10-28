384d8f053e4f6bf027b5affaca4dc0733020f7f2
njeveh <njeveh@gmail.com>
2021-10-28 16:04:49 +0300

    redefined the SellerSerializer class create method to allow for creation of nested objects

--- jengabay/serializers.py
+++ jengabay/serializers.py
@@ -20,6 +20,24 @@ class SellerSerializer(serializers.ModelSerializer):
         model = Seller
         fields = "__all__"
 
+    def create(self, validated_data):
+        subcounty_data = validated_data.pop("sub_county")
+        county_data = subcounty_data.pop("county")
+        try:
+            county = County.objects.get(county_name=county_data["county_name"])
+        except:
+            county = County.objects.create(**county_data)
+        subcounty_data.update({"county": county})
+
+        try:
+            sub_county = SubCounty.objects.get(subcounty_name=subcounty_data["subcounty_name"])
+        except:
+            sub_county = SubCounty.objects.create(**subcounty_data)
+        validated_data.update({"sub_county": sub_county})
+
+        return Seller.objects.create(**validated_data)
+
+
 class BuyerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Buyer
