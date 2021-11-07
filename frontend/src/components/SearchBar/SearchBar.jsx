4cf50676767e66cdcf5e5fdfba9ce7f9ceaf4876
Sau Juliana <julienyange@gmail.com>
2021-11-08 02:32:59 +0300

    added product page

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input"
-import {AiOutlineSearch} from "react-icons/ai"
+import {BiSearchAlt2} from "react-icons/bi"
 
 const SearchBar = () => {
     return(
         <InputGroup marginLeft="70px">
             <InputLeftElement
                 pointerEvents="none"
-                children={<AiOutlineSearch/>}
+                children={<BiSearchAlt2 w={4} />}
             />
             <Input background="#ffffff" borderRadius="50px" width="700px" placeholder="search here..." size="md"/>
         </InputGroup>
--- frontend/src/components/shared/NavBar.jsx
+++ frontend/src/components/shared/NavBar.jsx
@@ -1,11 +1,14 @@
 import React, {useState} from "react"
-import { Flex, HStack, Spacer } from "@chakra-ui/layout";
+import { Divider, Flex, HStack, Spacer } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/image";
 import icon from "../../assets/logo.png"
-import { Button} from "@chakra-ui/button";
+// import { Button} from "@chakra-ui/button";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
-import {BiMenu,BiMenuAltLeft} from "react-icons/bi"
+import {BiMenu, BiMenuAltLeft, BiCartAlt} from "react-icons/bi"
+import {IoIosArrowDown} from "react-icons/io"
+import {MdOutlineAccountCircle} from "react-icons/md"
+import {IoMdNotificationsOutline} from "react-icons/io"
 import { Icon } from "@chakra-ui/icon";
 
 const NavBar = () => {
@@ -20,10 +23,18 @@ const NavBar = () => {
             
             <SearchBar />
             <Spacer/>
-            <HStack spacing="10px">
+            {/* <HStack spacing="10px">
                 <Link to="/sign-up"><Button h="30px" alignItems="center" fontWeight="500" fontSize="13px" w="130px" textColor="#18A0FB" background="#ffffff" variant="outline">Sign Up</Button></Link>
-             <Link to="/sign-in"  ><Button h="30px" w="130px" alignItems="center" fontWeight="500" fontSize="13px" textColor="#ffffff" colorScheme="#18A0FB" background="#18A0FB" variant="solid">Login</Button></Link>
-            </HStack> 
+                <Link to="/sign-in"  ><Button h="30px" w="130px" alignItems="center" fontWeight="500" fontSize="13px" textColor="#ffffff" colorScheme="#18A0FB" background="#18A0FB" variant="solid">Login</Button></Link>
+            </HStack>  */}
+            <HStack spacing="30px">
+                <Icon color="#ffffff" h={7} w={7} as={IoMdNotificationsOutline}/>
+                <Icon color="#ffffff" h={7} w={7} as={BiCartAlt}/>
+                <Divider variant="solid" size="10px" orientation="vertical" /><HStack>
+                <Icon color="#ffffff" h={7} w={7} as={MdOutlineAccountCircle}/>
+                <Icon color="#ffffff" h={5} w={4} as={IoIosArrowDown}/></HStack>
+
+            </HStack>
         </Flex>
         
     )
