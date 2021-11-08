231609fc37d63061ce5089a8ed2d42efb9e8c995
Sau Juliana <julienyange@gmail.com>
2021-11-08 03:02:11 +0300

    made changes to the nav bar

--- frontend/src/components/shared/NavBar.jsx
+++ frontend/src/components/shared/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import { Divider, Flex, HStack, Spacer } from "@chakra-ui/layout";
+import { Box, Divider, Flex, HStack, Spacer } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/image";
 import icon from "../../assets/logo.png"
 // import { Button} from "@chakra-ui/button";
@@ -14,6 +14,15 @@ import { Icon } from "@chakra-ui/icon";
 const NavBar = () => {
     const [showMenu, setShowMenu] = useState(true)
     const handleClick = () => setShowMenu(!showMenu);
+    // const ColoredLine = ({color}) => (
+    //     <hr
+    //         style={{
+    //             color: color,
+    //             backgroundColor: color,
+    //             height: 5
+    //         }}
+    //     />
+    // );
     return(
         <Flex height="50px" pl={10} pr={10} alignItems="center" background="#007ACC">
         {showMenu ? <Icon onClick={handleClick} color="#FFFFFF" mr={5} as={BiMenu} h={9} w={9}/>: <Icon onClick={handleClick} color="#FFFFFF" mr={5} as={BiMenuAltLeft} h={9} w={9}/>}
@@ -27,10 +36,15 @@ const NavBar = () => {
                 <Link to="/sign-up"><Button h="30px" alignItems="center" fontWeight="500" fontSize="13px" w="130px" textColor="#18A0FB" background="#ffffff" variant="outline">Sign Up</Button></Link>
                 <Link to="/sign-in"  ><Button h="30px" w="130px" alignItems="center" fontWeight="500" fontSize="13px" textColor="#ffffff" colorScheme="#18A0FB" background="#18A0FB" variant="solid">Login</Button></Link>
             </HStack>  */}
-            <HStack spacing="30px">
+            <HStack spacing="20px">
                 <Icon color="#ffffff" h={7} w={7} as={IoMdNotificationsOutline}/>
                 <Icon color="#ffffff" h={7} w={7} as={BiCartAlt}/>
-                <Divider variant="solid" size="10px" orientation="vertical" /><HStack>
+                {/* <Box color="#ffffff" width="5px"></Box> */}
+                {/* <Divider variant="solid" size="10px" orientation="vertical" /> */}
+                {/* <ColoredLine color="red"/> */}
+                {/* <div style={{ borderTop: "2px solid #fff ", marginLeft: 20, marginRight: 20 }}></div> */}
+
+                <HStack>
                 <Icon color="#ffffff" h={7} w={7} as={MdOutlineAccountCircle}/>
                 <Icon color="#ffffff" h={5} w={4} as={IoIosArrowDown}/></HStack>
 
