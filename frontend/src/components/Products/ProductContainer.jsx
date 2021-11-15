47ab3517259100fa0c372af96e93ccef3805b88a
Sau Juliana <julienyange@gmail.com>
2021-11-15 17:26:33 +0300

    Tries X

--- frontend/src/components/Products/ProductContainer.jsx
+++ frontend/src/components/Products/ProductContainer.jsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/layout";
+import { Flex, Text } from "@chakra-ui/layout";
 import React from "react";
 import ProductCard from "./ProductCard";
 // import ProductList from "./ProductList";
@@ -8,10 +8,10 @@ const ProductContainer = ({handleAddProduct, productList}) =>{
         <Flex ml="5vw" borderRadius="10px" width="90vw" alignSelf="center" flexWrap="wrap" pl={3} pr={3}>
             {productList.map((product)=>{ 
             return(
-                <ProductCard product={product} handleAddProduct={handleAddProduct} id={product.id} company_image={product.item_seller.profile_pic} photo={product.item_main_image} category={product.category} name={product.item_name} description={product.item_description} companyName={product.item_seller.business_name}/> 
+                <ProductCard sellerId={product.item_seller} product={product} handleAddProduct={handleAddProduct} id={product.id} company_image={product.item_seller.profile_pic} photo={product.item_main_image} category={product.category} name={product.item_name} description={product.item_description} companyName={product.item_seller.business_name}/> 
             )
             })}
-            
+            {productList.length === 0 && <Text p={20}>There are no products here</Text> }
         </Flex>
     )
 }
