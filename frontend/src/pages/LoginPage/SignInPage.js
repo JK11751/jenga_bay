d2c0c5f738b1418b3c41a05f3001056413adda48
John <kgimase@gmail.com>
2021-11-17 09:44:54 +0300

    deleted node modules and yarn lock

--- frontend/src/pages/LoginPage/SignInPage.js
+++ frontend/src/pages/LoginPage/SignInPage.js
@@ -4,10 +4,9 @@ import { Box } from "@chakra-ui/layout";
 import SignInContainer from "./subs/SignInContainer";
 
 
-const SignInPage =({cartItems})=>{
+const SignInPage =()=>{
     return(
         <Box width="100vw" h="100vh" backgroundColor="#F8F8F8">
-            <NavBar cartItems={cartItems} />
             <SignInContainer />
            
         </Box>
