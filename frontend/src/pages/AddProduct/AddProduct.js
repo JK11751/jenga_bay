0a1ff26de37ce7a922c41be948911561ee1e3bce
Sau Juliana <julienyange@gmail.com>
2022-01-13 18:05:11 +0300

    Completed add Product functionlaity and updated Ui

--- frontend/src/pages/AddProduct/AddProduct.js
+++ frontend/src/pages/AddProduct/AddProduct.js
@@ -1,18 +1,41 @@
-import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Box, Flex, Avatar, Text } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/image";
 import UploadForm from "./UploadForm";
 import logo from "../../assets/JengaBay.png"
+import { useDispatch, useSelector } from 'react-redux';
+import { handleGetSellerDetails } from "../../redux/appActions/sellerActions";
+import { useParams } from "react-router-dom";
+
+
 const AddProduct=()=> {
+
+  const dispatch = useDispatch()
+  const seller_id = useParams()
+  const sellerDetails = useSelector((state) => state.sellerReducer).sellerDetails;
+
+  useEffect(() => {
+   dispatch(handleGetSellerDetails(seller_id.SellerId))
+  }, [dispatch,seller_id.SellerId])
+
   return (
     <Box >
       <Flex width="70vw">
         <Box
-          height="100vh"
+          height="150vh"
           width="25%"
           background="#007ACC"
         >
-          <Image ml={5} mt={5} src={logo} alt="logo" />
+          <Image ml={5} mt={5} mb={10} src={logo} alt="logo" />
+          {
+            sellerDetails.map((sellerDetails)=>{
+            return(
+              <>
+                <Avatar ml={10} mb={10} borderColor="#0095F8" borderWidth="5px" height="180px" width="180px" name={sellerDetails.business_name} src={sellerDetails.profile_pic}/>
+                <Text ml={10}>{sellerDetails.business_name}</Text>
+              </>
+            )})
+          }
         </Box>
         <Box height="100vh" width="70%" bg="#ffffff" >
             <UploadForm alignSelf="center"/>
--- frontend/src/pages/AddProduct/UploadForm.jsx
+++ frontend/src/pages/AddProduct/UploadForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { Textarea } from "@chakra-ui/react"
 import { Flex, Box, HStack, VStack } from "@chakra-ui/layout";
 import {
@@ -7,51 +8,139 @@ import {
   Button,
   FormControl,
   FormLabel,
+  Select,
 } from "@chakra-ui/react";
+import Categories from "../../data/CategoryList"
+import { useDispatch } from 'react-redux';
+import { handleAddProductSeller } from "../../redux/appActions/productActions";
+import { useParams } from "react-router-dom";
 
-const UploadForm =() =>{
+const UploadForm =() => {
+
+  const dispatch = useDispatch()
+  const seller_id = useParams()
+
+  const [itemName, setItemName] = useState("")
+  const [category, setCategory] = useState("metal and steel work")
+  const [description, setDescription] = useState("")
+  const [pricePerUnit, setPricePerUnit] = useState(null)
+  const [unit, setUnit] = useState("")
+  const [mainImage, setMainImage] = useState(null)
+  const [image1, setImage1] = useState(null)
+  const [image2, setImage2] = useState(null)
+  const [image3, setImage3] = useState(null)
+  const [image4, setImage4] = useState(null)
+
+  const data = {
+    "item_name": itemName,
+    "item_description": description,
+    "item_price": pricePerUnit,
+    "item_measurement_unit": unit,
+    "item_main_image": mainImage,
+    "item_extra_image1": image1,
+    "item_extra_image2": image2,
+    "item_extra_image3": image3,
+    "item_extra_image4": image4,
+    "category": category
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+    console.log(seller_id.SellerId)
+    let form_data = new FormData();
+    form_data.append("item_name", itemName)
+    form_data.append("item_description", description);
+    form_data.append("item_price", pricePerUnit);
+    form_data.append("item_measurement_unit", unit);
+    form_data.append("item_main_image", mainImage, mainImage.name);
+    form_data.append("item_extra_image1", image1, image1?.name);
+    form_data.append("item_extra_image2", image2, image2?.name);
+    form_data.append("item_extra_image3", image3, image3?.name);
+    form_data.append("item_extra_image3", image4, image4?.name);
+    form_data.append("category", category);
+
+    dispatch(handleAddProductSeller(seller_id.SellerId, form_data))
+  }
+
   return (
     <Flex flexDirection="column">
       <Box alignContent="center" ml="40px">
-        <VStack mt="50px" mb="50px" ml="300px">
-          <Button  
-          borderRadius="10px"
-          borderStyle="solid"
-          borderWidth="1px"
-          borderColor="#007ACC"
-          
-          > Upload photo(s)</Button>
-          <Flex>
-          <Text>You may upload upto five photos. Please upload quality photos</Text>
-          </Flex>
-          </VStack>
-      <HStack mb="50px">
+      <Text fontSize={20} m={10}>Add Product</Text>
+      <VStack mb="100px">
           <FormControl id="product-Name" isRequired>
             <FormLabel>Product Name</FormLabel>
-            <Input variant="filled" width="300px" />
+            <Input onChange={e=>setItemName(e.target.value)} variant="filled" width="100%" />
           </FormControl>
           <FormControl id="category" isRequired>
             <FormLabel>Category</FormLabel>
-            <Input variant="filled" width="300px" />
+            <Select onChange={e=>{setCategory(e.target.value)}}>
+                {
+                  Categories.map((category) => 
+                    {
+                      return(<option>{category.value}</option>)
+                    }
+                  )
+                }
+            </Select>
+            {/* <Input variant="filled" width="100%" /> */}
           </FormControl>
-    </HStack>
-        <HStack mb="50px">
-          <FormControl id="unit" isRequired>
-            <FormLabel>Unit</FormLabel>
-            <Input variant="filled" width="300px" />
-          </FormControl>
-          <FormControl id="price" isRequired>
-            <FormLabel>Price Range</FormLabel>
-              <Input variant="filled" width="300px" />
-          </FormControl>
-          
-        </HStack>
-        <HStack mb="20px">
           <FormControl id="product-description" isRequired>
             <FormLabel>Product description</FormLabel>
-            <Textarea variant="filled" width="800px" height="220px"/>
+            <Textarea onChange={e=>setDescription(e.target.value)} variant="filled" width="100%" height="220px"/>
           </FormControl>
-        </HStack>
+    
+          <HStack alignItems="left">
+            <FormControl id="price" isRequired>
+              <FormLabel>Price per Unit</FormLabel>
+              <Input onChange={e=>setPricePerUnit(e.target.value)} variant="filled" width="100%" />
+            </FormControl>
+            <FormControl id="unit" isRequired>
+              <FormLabel>Unit</FormLabel>
+              <Input onChange={e=>setUnit(e.target.value)} variant="filled" width="100%" />
+            </FormControl>
+          </HStack> 
+          <Box as="span" alignSelf="left" fontSize={20} m={10}>Product Images</Box>
+          <Text fontSize={10} m={10}>You may upload up to 5 photos. Please upload good quality photos</Text>   
+          <FormControl id="price" isRequired>
+            <FormLabel>Item Main Image</FormLabel>
+            <input type="file" accept="image/png, image/jpeg image/jpg" onChange={e=>{setMainImage(e.target.files[0])}} variant="filled" width="100%" />
+          </FormControl> 
+          <HStack alignItems="left">
+            <FormControl id="price" >
+              <FormLabel>Item Extra Image 1</FormLabel>
+              <input type="file" accept="image/png, image/jpeg" onChange={e=>{setImage1(e.target.files[0])}} variant="filled" width="100%" />
+            </FormControl>
+            <FormControl id="unit" >
+              <FormLabel>Item Extra Image 2</FormLabel>
+              <input type="file" accept="image/png, image/jpeg" onChange={e=>{setImage2(e.target.files[0])}} variant="filled" width="100%" />
+            </FormControl>
+          </HStack> 
+          <HStack alignItems="left">
+            <FormControl id="price">
+              <FormLabel>Item Extra Image 3</FormLabel>
+              <input type="file" accept="image/png, image/jpeg" onChange={e=>{setImage3(e.target.files[0])}}  variant="filled" width="100%" />
+            </FormControl>
+            <FormControl id="unit">
+              <FormLabel>Item Extra Image 4</FormLabel>
+              <input type="file" accept="image/png, image/jpeg" onChange={e=>{setImage4(e.target.files[0])}}  variant="filled" width="100%" />
+            </FormControl>
+          </HStack> 
+        </VStack>
+        <Button
+            padding="10px"
+            background="#007ACC"
+            width="200px"
+            height="40px"
+            borderRadius="50px"
+            borderWidth="1px"
+            borderColor="#007ACC"
+            borderStyle="solid "
+            color="#ffffff"
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
       </Box>
     </Flex>
   );
